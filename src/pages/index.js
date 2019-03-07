52b7860a6bee99cd9b6f58910c15e4633dd9f6a6
Nicholas Rutherford <rutherfn@gmatc.matc.edu>
2019-03-07 16:37:21 -0600

    Created custom components, that will be used

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
-import React from "react"
-import { Link } from "gatsby"
-
+import React from 'react'
+import { Link } from 'gatsby'
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import SEO from '../components/seo'
+import { Button, IconButton, Section } from '../components/Element'
+import { Search } from 'styled-icons/feather'
+import { Footer } from '../components/Element/Footer'
 
 import MainPage from '../components/MainPage';
 
@@ -11,10 +13,11 @@ const IndexPage = () => (
    
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <MainPage>
-      
     </MainPage>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
     </div>
+    <Footer>
+</Footer>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
